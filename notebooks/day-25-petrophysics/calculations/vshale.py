26e46b5d5d0b00a6f6297abdc3c59fa87fceaf86
Md Ashraf <23mc0049@iitism.ac.in>
2026-04-15 20:45:17 +0530

    Update Python scripts

--- notebooks/day-25-petrophysics/calculations/vshale.py
+++ notebooks/day-25-petrophysics/calculations/vshale.py
@@ -6,7 +6,7 @@ import numpy as np
 def compute_vsh_gr(gr, gr_min=None, gr_max=None, model: str = "linear"):
     gr = np.asarray(gr, dtype=float)
     if gr_min is None:
-        gr_min = np.nanpercentile(gr, 5)
+        gr_min = np.nanpercentile(gr, 5)  # GR Clean
     if gr_max is None:
         gr_max = np.nanpercentile(gr, 95)
     igr = (gr - gr_min) / (gr_max - gr_min + 1e-9)
