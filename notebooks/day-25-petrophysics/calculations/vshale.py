b6b4f5d505d3c72df3235b48427d82485ae48b65
Md Ashraf <23mc0049@iitism.ac.in>
2026-04-15 23:15:32 +0530

    Update Python scripts

--- notebooks/day-25-petrophysics/calculations/vshale.py
+++ notebooks/day-25-petrophysics/calculations/vshale.py
@@ -26,5 +26,5 @@ def compute_vsh_gr(gr, gr_min=None, gr_max=None, model: str = "linear"):
         vsh = igr / (3.0 - 2.0 * igr + 1e-9)
     else:
         vsh = igr
-
+        
     return np.clip(vsh, 0.0, 1.0)
