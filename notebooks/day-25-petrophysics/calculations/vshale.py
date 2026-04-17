35b3e98a696d48460b42091d065bb05b1e8e8f22
Md Ashraf <23mc0049@iitism.ac.in>
2026-04-17 11:53:43 +0530

    Update Python scripts

--- notebooks/day-25-petrophysics/calculations/vshale.py
+++ notebooks/day-25-petrophysics/calculations/vshale.py
@@ -20,7 +20,11 @@ def compute_vsh_gr(gr, gr_min=None, gr_max=None, model: str = "linear"):
         # Larionov for older rocks
         vsh = 0.33 * (2 ** (2.0 * igr) - 1.0)
     elif "clavier" in name:
+        # second_term = 3.38 - np.square(igr + 0.7)
+        # vsh = 1.7 - np.pow(second_term,0.5)
+
         second_term = 3.38 - np.square(igr + 0.7)
+        second_term = np.maximum(second_term, 0.0)
         vsh = 1.7 - np.sqrt(second_term)
     elif "steiber" in name: 
         vsh = igr / (3.0 - 2.0 * igr + 1e-9)
