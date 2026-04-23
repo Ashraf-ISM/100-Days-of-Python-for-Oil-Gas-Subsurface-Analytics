53d3a72d8285051d04c3d87ae3e4a4b4fed972ee
Md Ashraf <23mc0049@iitism.ac.in>
2026-04-23 23:55:30 +0530

    Update Python scripts

--- notebooks/day-25-petrophysics/plotting/plot_tools.py
+++ notebooks/day-25-petrophysics/plotting/plot_tools.py
@@ -455,6 +455,14 @@ def plot_multitrack(
                     edgecolor="none",
                     pad=2
                 ))
+            # Fill caliper log
+            ax.fill_betweenx(
+                depth[mask],
+                0,                      # fill from GR baseline
+                values[mask],
+                color="purple",
+                alpha=0.30
+            )
 
         #  GR shading
         if any(curve_upper.startswith(k) for k in GR_KEYS):
