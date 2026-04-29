ba84c604e72fcff6e9a219d3c0db0d7ca2ada42f
Md Ashraf <23mc0049@iitism.ac.in>
2026-04-29 23:52:37 +0530

    Update Python source files

--- notebooks/day-25-petrophysics/ai_analytics/missing_log/training_controller.py
+++ notebooks/day-25-petrophysics/ai_analytics/missing_log/training_controller.py
@@ -15,7 +15,7 @@ from PyQt5 import QtCore, QtWidgets
 
 
 # ──────────────────────────────────────────────────────────────────────────────
-# Background worker thread and 
+# Background worker thread and training controller 
 # ──────────────────────────────────────────────────────────────────────────────
 
 class TrainingWorker(QtCore.QThread):
