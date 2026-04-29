2a25e9ee7baea9096c03d7f9a82a5b221c6f1c24
Md Ashraf <23mc0049@iitism.ac.in>
2026-04-29 23:56:42 +0530

    Update Python source files

--- notebooks/day-25-petrophysics/ai_analytics/missing_log/training_controller.py
+++ notebooks/day-25-petrophysics/ai_analytics/missing_log/training_controller.py
@@ -271,3 +271,4 @@ def _set(win, name: str, val: str, index: int = 0):
     widgets = win.findChildren(QtWidgets.QLabel, name)
     if widgets and index < len(widgets):
         widgets[index].setText(val)
+        
