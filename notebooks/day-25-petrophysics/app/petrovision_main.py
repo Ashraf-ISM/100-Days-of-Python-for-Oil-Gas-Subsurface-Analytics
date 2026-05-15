7c89ae1584e036a0cbde42e4defbbb7fb3cd905f
Md Ashraf <23mc0049@iitism.ac.in>
2026-05-16 01:43:52 +0530

    Update Python source files

--- notebooks/day-25-petrophysics/app/petrovision_main.py
+++ notebooks/day-25-petrophysics/app/petrovision_main.py
@@ -235,7 +235,7 @@ class PetroVisionMainWindow(QtWidgets.QMainWindow):
         self._dashboard_quick_buttons = [
             btn_add_well,
             btn_import_well,
-            self._make_launch_button("Load Demo Data" , self._load_demo_data, "#FF6B6B"),
+            self._make_launch_button("Load Demo Data", self._load_demo_data, "#FF6B6B"),
             self._make_launch_button("Data Downloader", self._open_data_downloader, "#5E35B1"),
             self._make_launch_button("Log Viewer", lambda: self._call_controller_action("_go_to_logviewer_tab"), "#1FA67A"),
             self._make_launch_button("Crossplot", lambda: self._trigger_widget_click("btnDashXplot"), "#D48A1D"),
