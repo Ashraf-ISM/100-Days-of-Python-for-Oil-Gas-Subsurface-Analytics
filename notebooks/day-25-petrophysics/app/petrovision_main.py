890ba5f9f4db6bd3760e3b2ce5182c0986711306
Md Ashraf <23mc0049@iitism.ac.in>
2026-05-29 23:47:04 +0530

    Update Python source files

--- notebooks/day-25-petrophysics/app/petrovision_main.py
+++ notebooks/day-25-petrophysics/app/petrovision_main.py
@@ -1494,7 +1494,7 @@ class PetroVisionMainWindow(QtWidgets.QMainWindow):
 def main():
     app = QtWidgets.QApplication(sys.argv)
 
-    # Set App Icon 
+    # Set App Icon for showing 
     logo_path = ASSETS_DIR / "logo-petroarx.png"  
     if logo_path.exists():
         app.setWindowIcon(QtGui.QIcon(str(logo_path)))
