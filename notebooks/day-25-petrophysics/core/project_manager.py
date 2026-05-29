33b888867c6bec10faaddad3ae1292fc7d888db5
Md Ashraf <23mc0049@iitism.ac.in>
2026-05-29 23:49:01 +0530

    Update Python source files

--- notebooks/day-25-petrophysics/core/project_manager.py
+++ notebooks/day-25-petrophysics/core/project_manager.py
@@ -47,6 +47,7 @@ class ProjectData:
             "region": "",
             "country": "",
             "operator": "",
+            
         }
         # Names of wells encoded in the archive (populated on load)
         self.well_names: list[str] = []
