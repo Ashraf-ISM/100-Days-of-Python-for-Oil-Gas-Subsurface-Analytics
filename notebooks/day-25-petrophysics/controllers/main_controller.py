ed7e0a4cef01543d3f836fc93a202d96a6604663
Md Ashraf <23mc0049@iitism.ac.in>
2026-05-30 23:52:45 +0530

    Update Python source files

--- notebooks/day-25-petrophysics/controllers/main_controller.py
+++ notebooks/day-25-petrophysics/controllers/main_controller.py
@@ -86,7 +86,7 @@ class MainController:
         self._connect_action("actionFluidAnalysis", self._show_fluid_analysis_info) 
         self._connect_action("actionMudLog", self._show_mud_log_info)
         # Project Browser sidebar '+ Well' and 'Import' buttons 
-        self._connect_widget("btnBrowseAddWell", "clicked", self._import_and_track)
+        self._connect_widget("btnBrowseAddWell", "clicked", self._import_and_track) 
         self._connect_widget("btnBrowseImport", "clicked", self._import_and_track) 
 
         # Plotting
