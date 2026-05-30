124fc4e69e5c2c38de7762003ba9bac5444f8d5d
Md Ashraf <23mc0049@iitism.ac.in>
2026-05-30 23:52:40 +0530

    Update Python source files

--- notebooks/day-25-petrophysics/controllers/main_controller.py
+++ notebooks/day-25-petrophysics/controllers/main_controller.py
@@ -106,7 +106,7 @@ class MainController:
         self._connect_widget("advance_cross_plot", "clicked", self.plots.open_advanced_crossplot) 
         self._connect_widget("btnLVPlotHistogram", "clicked", self.plots.new_histogram)
         self._connect_widget("btnLVPlotPairplot", "clicked", self.plots.new_pairplot)
-        self._connect_widget("btnLVPlotViolin", "clicked", self.plots.new_violinplot)
+        self._connect_widget("btnLVPlotViolin", "clicked", self.plots.new_violinplot) 
         # Log viewer tab buttons
         self._connect_widget("plottriplecomboplot", "clicked", self.plots.new_triple_combo)
         self._connect_widget("plotmutlitrack", "clicked", self.plots.new_log_plot)
